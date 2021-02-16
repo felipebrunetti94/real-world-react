2da079378a554dcd108c9840a7a34f5debc17ca6
Felipe Brunetti <felipe.brunetti94@gmail.com>
2021-02-16 11:11:54 -0300

    feat(view): add routes editor, article, settings and user page

--- src/view/Router.js
+++ src/view/Router.js
@@ -3,6 +3,10 @@
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Layout from "./layout/Layout";
 import HomePage from "./home/HomePage";
+import EditorPage from "./article/EditorPage";
+import ArticlePage from "./article/ArticlePage";
+import SettingsPage from "./user/SettingsPage";
+import ProfilePage from "./user/ProfilePage";
 import LoginPage from "./auth/LoginPage";
 import RegisterPage from "./auth/RegisterPage";
 import PrivateOnlyRoute from "./auth/PrivateOnlyRoute";
@@ -16,7 +20,11 @@ const Router = () => {
           <Route path="/" component={HomePage} />
           <PublicOnlyRoute path="/login" component={LoginPage} />
           <PublicOnlyRoute path="/register" component={RegisterPage} />
-          <PrivateOnlyRoute path="/editor" component={() => <h1>Editor</h1>} />
+          <PrivateOnlyRoute path="/editor" component={EditorPage} />
+          <PrivateOnlyRoute path="/editor/:slug" component={EditorPage} />
+          <Route path="/article/:slug" component={ArticlePage} />
+          <Route path="/@:username" component={ProfilePage} />
+          <PrivateOnlyRoute path="/settings" component={SettingsPage} />
           <Redirect to="/" />
         </Switch>
       </Layout>
