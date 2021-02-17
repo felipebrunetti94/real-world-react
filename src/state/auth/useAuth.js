8c66e9b4ce86dcdba1e00560cdcfa122d8b2f990
Felipe Brunetti <felipe.brunetti94@gmail.com>
2021-02-16 23:23:08 -0300

    feat(state): add auth hook
    this hook handle all the logic involved in auth and user state

--- src/state/auth/useAuth.js
+++ src/state/auth/useAuth.js
@@ -1,34 +1,56 @@
-// @ts-check
-
-import { useReducer } from "react";
+import { useReducer, createContext, useContext, useMemo } from "react";
 import initialState from "./initialState";
 import * as AUTH from "./types";
 import reducer from "./reducer";
 
-const useAuth = ({ registerUser, loginUser }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+const AuthContext = createContext(null);
 
-  return {
-    ...state,
-    updateAuthInfo: (authInfo) =>
-      dispatch({ type: AUTH.UPDATE_AUTH_INFO, authInfo }),
-    onRegisterUser: (authInfo) => {
+export const AuthProvider = ({
+  children,
+  registerUser,
+  loginUser,
+  storage,
+}) => {
+  const [state, dispatch] = useReducer(
+    reducer,
+    storage.user ? storage : initialState
+  );
+
+  const value = useMemo(() => {
+    const onRegisterUser = (authInfo) => {
       dispatch({ type: AUTH.REGISTER_USER_REQUEST });
       return registerUser(authInfo, {
-        onSuccess: () => dispatch({ type: AUTH.REGISTER_USER_REQUEST_SUCCESS }),
+        onSuccess: (user) =>
+          dispatch({ type: AUTH.REGISTER_USER_REQUEST_SUCCESS, user }),
         onError: (errors) =>
           dispatch({ type: AUTH.REGISTER_USER_REQUEST_ERROR, errors }),
       });
-    },
-    onUserLogin: (authInfo) => {
+    };
+
+    const onUserLogin = (authInfo) => {
       dispatch({ type: AUTH.LOGIN_USER_REQUEST });
       return loginUser(authInfo, {
-        onSuccess: () => dispatch({ type: AUTH.LOGIN_USER_REQUEST_SUCCESS }),
+        onSuccess: (user) =>
+          dispatch({ type: AUTH.LOGIN_USER_REQUEST_SUCCESS, user }),
         onError: (errors) =>
           dispatch({ type: AUTH.LOGIN_USER_REQUEST_ERROR, errors }),
       });
-    },
-  };
+    };
+
+    const updateAuthInfo = (authInfo) =>
+      dispatch({ type: AUTH.UPDATE_AUTH_INFO, authInfo });
+
+    return {
+      ...state,
+      updateAuthInfo,
+      onRegisterUser,
+      onUserLogin,
+    };
+  }, [state, dispatch, registerUser, loginUser]);
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
+
+const useAuth = () => useContext(AuthContext);
 
 export default useAuth;
