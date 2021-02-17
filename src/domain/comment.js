e88f558bf77446c4f291e7fe9cbab2a7617b0db1
Felipe Brunetti <felipe.brunetti94@gmail.com>
2021-02-16 23:33:39 -0300

    feat(domain): add comment entity

---
+++ src/domain/comment.js
@@ -0,0 +1,12 @@
+import { Author } from "./author";
+
+/**
+ * @typedef Comment
+ * @property {number} id
+ * @property {Date} createdAt
+ * @property {Date} updatedAt
+ * @property {string} body
+ * @property {Author} author
+ */
+
+export default {};
