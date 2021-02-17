9521e1bafc7de730c23fc470fac7b3fe42fd204a
Felipe Brunetti <felipe.brunetti94@gmail.com>
2021-02-17 00:49:30 -0300

    refactor(article preview): update component to use Link from react router dom

--- src/view/article/ArticlePreview.js
+++ src/view/article/ArticlePreview.js
@@ -1,25 +1,35 @@
+import { Link } from "react-router-dom";
+
 const ArticlePreview = ({ article }) => {
+  const {
+    author,
+    createdAt,
+    favoritesCount,
+    slug,
+    title,
+    description,
+  } = article;
   return (
     <div className="article-preview">
       <div className="article-meta">
         <a href="profile.html">
-          <img src="http://i.imgur.com/Qr71crq.jpg" />
+          <img src={author.image} alt={`${author.username} profile`} />
         </a>
         <div className="info">
-          <a href="" className="author">
-            {article.author.username}
-          </a>
-          <span className="date">{article.createdAt}</span>
+          <Link to={`/@${author.username}`} className="author">
+            {author.username}
+          </Link>
+          <span className="date">{createdAt}</span>
         </div>
         <button className="btn btn-outline-primary btn-sm pull-xs-right">
-          <i className="ion-heart"></i> 29
+          <i className="ion-heart"></i> {favoritesCount}
         </button>
       </div>
-      <a href="" className="preview-link">
-        <h1>How to build webapps that scale</h1>
-        <p>This is the description for the post.</p>
+      <Link to={`/article/${slug}`} className="preview-link">
+        <h1>{title}</h1>
+        <p>{description}</p>
         <span>Read more...</span>
-      </a>
+      </Link>
     </div>
   );
 };
