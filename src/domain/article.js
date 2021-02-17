9dd001c43a1145915a2eb1ac7567365b8cae37ee
Felipe Brunetti <felipe.brunetti94@gmail.com>
2021-02-16 23:29:32 -0300

    feat(domain): add article entity

---
+++ src/domain/article.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef Article
+ * @property {string} slug
+ * @property {string} title
+ * @property {string} description
+ * @property {string} body
+ * @property {string[]} tagList
+ * @property {string} createdAt
+ * @property {string} updatedAt
+ * @property {string} title
+ */
+
+export default {};
