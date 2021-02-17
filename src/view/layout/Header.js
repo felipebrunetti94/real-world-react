512d17707373aba5d3326c162984717b7980ec1b
Felipe Brunetti <felipe.brunetti94@gmail.com>
2021-02-17 12:13:12 -0300

    feat(state): add sign out option

--- src/view/layout/Header.js
+++ src/view/layout/Header.js
@@ -1,56 +1,90 @@
 // @ts-check
 
 import { NavLink } from "react-router-dom";
+import useAuth from "../../state/auth/useAuth";
 
-const Header = ({ user }) => (
-  <header>
-    <nav className="navbar navbar-light">
-      <div className="container">
-        <a className="navbar-brand" href="index.html">
-          conduit
-        </a>
-        <ul className="nav navbar-nav pull-xs-right">
-          <li className="nav-item">
-            <NavLink className="nav-link" activeClassName="active" to="/" exact>
-              Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              className="nav-link"
-              activeClassName="active"
-              to="/editor"
-              exact
-            >
-              <i className="ion-compose"></i>&nbsp;New Post
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              className="nav-link"
-              activeClassName="active"
-              to="/settings"
-              exact
-            >
-              <i className="ion-gear-a"></i>&nbsp;Settings
-            </NavLink>
-          </li>
-          {user && (
+const Header = () => {
+  const { loggedIn, onSignOut } = useAuth();
+  return (
+    <header>
+      <nav className="navbar navbar-light">
+        <div className="container">
+          <a className="navbar-brand" href="index.html">
+            conduit
+          </a>
+          <ul className="nav navbar-nav pull-xs-right">
             <li className="nav-item">
               <NavLink
                 className="nav-link"
                 activeClassName="active"
-                to="/register"
+                to="/"
                 exact
               >
-                Sign up
+                Home
               </NavLink>
             </li>
-          )}
-        </ul>
-      </div>
-    </nav>
-  </header>
+
+            {loggedIn ? (
+              <PrivateOnlyNav onSignOut={onSignOut} />
+            ) : (
+              <PublicOnlyNav />
+            )}
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+const PublicOnlyNav = () => (
+  <>
+    <li className="nav-item">
+      <NavLink className="nav-link" activeClassName="active" to="/login" exact>
+        Sign in
+      </NavLink>
+    </li>
+    <li className="nav-item">
+      <NavLink
+        className="nav-link"
+        activeClassName="active"
+        to="/register"
+        exact
+      >
+        Sign up
+      </NavLink>
+    </li>
+  </>
+);
+
+const PrivateOnlyNav = ({ onSignOut }) => (
+  <>
+    <li className="nav-item">
+      <NavLink className="nav-link" activeClassName="active" to="/editor" exact>
+        <i className="ion-compose"></i>&nbsp;New Post
+      </NavLink>
+    </li>
+    <li className="nav-item">
+      <NavLink
+        className="nav-link"
+        activeClassName="active"
+        to="/settings"
+        exact
+      >
+        <i className="ion-gear-a"></i>&nbsp;Settings
+      </NavLink>
+    </li>
+    <li className="nav-item">
+      <NavLink
+        onClick={onSignOut}
+        className="nav-link"
+        activeClassName="active"
+        to="/login"
+        exact
+      >
+        Sign out
+      </NavLink>
+    </li>
+  </>
 );
 
 export default Header;
