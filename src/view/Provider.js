35f58bb44d643a25dcfc5a766f116b4441fafa84
Felipe Brunetti <felipe.brunetti94@gmail.com>
2021-02-16 23:25:38 -0300

    refactor(provider): rename sigin user to login user

--- src/view/Provider.js
+++ src/view/Provider.js
@@ -1,11 +1,11 @@
 import { AuthProvider } from "../state/auth/useAuth";
 
 const Provider = ({ storage, container, children }) => {
-  const { registerUser, signinUser } = container;
+  const { registerUser, loginUser } = container;
   return (
     <AuthProvider
       registerUser={registerUser}
-      signinUser={signinUser}
+      loginUser={loginUser}
       storage={storage}
     >
       {children}
